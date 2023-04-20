2ff1ec837558b730161e36ac6bc00100829a5b1a
xzen769 <xzen769@gmail.com>
2023-04-20 16:10:00 +0200

    test(login): add perf test on login endpoint

--- api/app/config.js
+++ api/app/config.js
@@ -2,11 +2,11 @@ module.exports = {
   development: {
     type: 'development',
     port: 3000,
-    mongodb: ''
+    mongodb: 'mongodb+srv://12345:12345@demo.v4muu5b.mongodb.net/cybersecuritytp'
   },
   production: {
     type: 'production',
     port: 3000,
-    mongodb: ''
+    mongodb: 'mongodb+srv://12345:12345@demo.v4muu5b.mongodb.net/cybersecuritytp'
   }
-}
+};
